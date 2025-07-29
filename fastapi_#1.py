8684f87efd3ffc679749925b450228613372d2ee
Kuhu <vyaskuhu1@gmail.com>
2025-07-30 04:28:43 +0530

    put request type on /tasks/task_id
    setup PUT on '/tasks/task_id' with triggering function for updating the task

--- fastapi_#1.py
+++ fastapi_#1.py
@@ -30,3 +30,12 @@ def read_task(task_id: UUID):
             return task
 
     raise HTTPException(status_code=404, detail="Task not found")
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: UUID, task_update: Task):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(update=task_update.dict(exclude_unset=True))
+            tasks[idx] = updated_task
+            return updated_task
