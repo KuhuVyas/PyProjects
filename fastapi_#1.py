08afe38f50a0abd0bb33b52de02eebf336cd30fa
Kuhu <vyaskuhu1@gmail.com>
2025-07-30 04:15:23 +0530

    instanciated and created  base class
    Here I've created a fastapi instance and a wrapper basemodel class "Task"

--- fastapi_#1.py
+++ fastapi_#1.py
@@ -3,5 +3,10 @@ from typing import Lists, Dicts, Optionals
 from fastapi import FastAPI 
 from uuid import UUID, uuid4
 
-
+app = FastAPI()
+class Task(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
 
