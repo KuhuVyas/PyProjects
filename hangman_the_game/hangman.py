d244769edc4fdc7d54105888230158a954b59e3a
KuhuVyas <vyaskuhu1@gmail.com>
2025-08-06 09:41:05 +0530

    chosing a word

--- hangman_the_game/hangman.py
+++ hangman_the_game/hangman.py
@@ -1,3 +1,10 @@
 import random
 from hangman_words import word_list
 from hangman_art import stages, logo
+#TODO: Setting the lives, as per the stages in the hangman game 
+lives = 6
+
+# TODO: - Import the logo from hangman_art.py and print it at the start of the game.
+print(logo)
+chosen_word = random.choice(word_list)
+print(chosen_word)
